8aeefa47175c30ad37fe68189948461f769ec8d9
SofiDev <angiemeow89@gmail.com>
2024-01-05 23:15:39 -0800

    Se agregan funciones primarias

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-SofiDev Oficial Theme
+<?php
+  function greet($name, $petName){
+    echo "<p>Hi, my name is $name and I like $petName</p>";
+  }
+  greet("Sofi", "cats");
+  greet("Angie", "Snakes");
+
+?>
+<h1><?php blogInfo('name') ?></h1>
+<p><?php blogInfo("description")?></p>
